dbea591f0725276c9c036722fdc6edb69f4df9d5
Kush Chhatbar <kushc@200oksolutions.com>
2025-11-24 15:15:44 +0530

    first program in python

---
+++ prac1.py
@@ -0,0 +1,15 @@
+""""
+Program to add two numbers.
+
+"""
+num1 =  34
+num2 = 56
+
+sum = num1 + num2
+print("Sum of", num1, "and", num2, "is:", sum)
+
+
+"""
+Program to subtract two numbers.
+
+"""
